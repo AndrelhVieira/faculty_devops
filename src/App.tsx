eb4c0587fab8b3382ea541d96775664dd8710294
Andre Vieira <db1.andre.vieira@parceirocantu.com.br>
2025-04-18 13:36:40 -0300

    feat: update index.html language and title; implement task input and display in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <input type="text" name="task" placeholder="Add a new task" />
+        <button type="button">Add</button>
+      </div>
+
+      <div>
+        <h2>Tasks</h2>
+
+        <ul>
+          <li>
+            <p>Task 1</p>
+            <button type="button">Delete</button>
+          </li>
+          <li>
+            <p>Task 2</p>
+            <button type="button">Delete</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
